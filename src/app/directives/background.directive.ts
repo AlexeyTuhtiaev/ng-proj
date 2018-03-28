65ad980d2738533fd42cf19a91483751fea7d907
Alexey.Tuhtiaev <Alexey.Tuhtiaev@gmail.com>
2018-03-28 12:53:38 +0300

    add parameters to directives

--- src/app/directives/background.directive.ts
+++ src/app/directives/background.directive.ts
@@ -1,26 +1,25 @@
-import {Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2} from '@angular/core';
+import {Directive, ElementRef, Input, HostBinding, HostListener, OnInit, Renderer2} from '@angular/core';
+
 @Directive({
   selector: '[appBackground]'
 })
 export class BackgroundDirective implements OnInit {
+  @Input ('appBackground') hoverColor: string = 'green';
+  @Input () defaultColor: string = 'transparent';
+
 
   @HostBinding ('style.backgroundColor') background: string;
 
   constructor(private element: ElementRef, private renderer: Renderer2) {}
   ngOnInit() {
-    // const {nativeElement} = this.element;
-    // this.renderer.setStyle(nativeElement, 'background-color', 'blue');
-    // this.renderer.addClass(nativeElement, 'white-text');
-    // this.element.nativeElement.style.backgroundColor='red';
+    this.background=this.defaultColor;
   }
 
   @HostListener('mouseenter') mouseEnter(event: Event) {
-    // this.renderer.setStyle(this.element.nativeElement, 'background-color', 'red');
-    this.background='green';
+    this.background=this.hoverColor;
 
   }
   @HostListener('mouseleave') mouseLeave(event: Event) {
-    // this.renderer.setStyle(this.element.nativeElement, 'background-color', 'transparent');
-    this.background='transparent';
+    this.background=this.defaultColor;
   }
 }
